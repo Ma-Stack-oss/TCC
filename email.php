f0cc251b071ee197ae1cf10412eefa81cdeadbce
Matheus <Matheus.22082004673@outlook.com>
2021-10-21 05:46:47 -0300

    UFA

---
+++ email.php
@@ -0,0 +1,11 @@
+<?php
+
+if(isset($_POST['email']) && !empty($_POST['email'])
+
+$nome = addslashes($_POST['name'])
+$email = addslashes($_POST['email'])
+$mensagem = addslashes($_POST['message'])
+
+}
+
+?>
